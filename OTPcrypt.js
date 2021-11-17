e1596a182cf041c23bbdab85d7d687608fc09723
Mackain <markus.j.lindgren@gmail.com>
2021-11-17 09:41:05 +0100

    longer keys cuz it looks better

--- OTPcrypt.js
+++ OTPcrypt.js
@@ -1,7 +1,7 @@
 // Put all valid characters into this array. Any character not part of this array will not get encrypted.
 const alpha = [' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', ',', '!','?','&','+','-',':',';','(',')','/','\\','>']
 const groupLen = 16; // number of characters per group
-const groupNumb = 25; // number of groups per key
+const groupNumb = 30; // number of groups per key
 
 // also, lets have one version of the space array without the special stuff... for AESTETICS
 const spacelessAlpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
